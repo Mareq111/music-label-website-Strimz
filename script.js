dfe75b99cc2252424262b0e657aa7a1fbb158ef4
marq sz <marekszulc2020@gmail.com>
2023-10-19 15:02:50 +0200

    feat add animated to toggle menu

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+//toggle menu
+const menuIcon = document.getElementById("dropdown-menu-icon");
+const toggleMenu = document.getElementById("toggle_menu");
 
+menuIcon?.addEventListener("click", () => {
+  toggleMenu?.classList.toggle("active");
+});
