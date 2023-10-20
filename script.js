3b63b3f5cc9fcfa2dd656fc70da5b3f8681b1804
marq sz <marekszulc2020@gmail.com>
2023-10-20 11:46:54 +0200

    feat add animations to logo and  buttons for songs

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const toggleMenu = document.getElementById("toggle_menu");
 menuIcon?.addEventListener("click", () => {
   toggleMenu?.classList.toggle("active");
 });
+
+//if logo click go to top
+
+const logoClick = document.getElementById("logo");
+
+logoClick?.addEventListener("click", () => {
+  window.scroll({ top: 0, behavior: "smooth" });
+});
+
+//? test btn
+
+const playBtn = document.getElementById("playBtn");
