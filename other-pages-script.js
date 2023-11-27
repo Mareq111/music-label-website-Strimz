2806b3c23d7596c51985e215e60a30ac4328e6a5
marq sz <marekszulc2020@gmail.com>
2023-11-27 18:06:53 +0100

    feat: add chooser for months in js and sticky scroll btns-chooser in tour page

--- other-pages-script.js
+++ other-pages-script.js
@@ -27,15 +27,52 @@ summaryFocus?.addEventListener("click", () => {
 
 //3 month chooser for tour page
 
+/* //dziala
 const buttonChooser = document.querySelectorAll(".btn-month");
+const monthDivs = document.querySelectorAll(".month-div");
 const may = document.getElementById("btn-May");
 const june = document.getElementById("btn-June");
 const july = document.getElementById("btn-July");
 const august = document.getElementById("btn-August");
-
+// btns per month
 buttonChooser.forEach((button) => {
   button.addEventListener("click", () => {
     document.querySelector(".btn-month.active")?.classList.remove("active");
     button.classList.add("active");
+
+    // dates per month card
+    const monthId = button.id.split("-")[1].toLowerCase();
+
+    monthDivs.forEach((div) => {
+      div.classList.remove("active");
+    });
+
+    //add 'active' class
+    const selectedMonthDiv = document.getElementById(`${monthId}-card`);
+    selectedMonthDiv.classList.add("active");
+  });
+}); */
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonChooser = document.querySelectorAll(".btn-month");
+  const monthDivs = document.querySelectorAll(".month-div");
+  // btns per month
+  buttonChooser.forEach((button) => {
+    button.addEventListener("click", () => {
+      document.querySelector(".btn-month.active")?.classList.remove("active");
+      button.classList.add("active");
+
+      // dates per month card and split btn id into array with 2 elements
+      const monthId = button.id.split("-")[1];
+
+      monthDivs.forEach((div) => {
+        div.classList.remove("active");
+      });
+      //add 'active' class
+      const selectedMonthDiv = document.getElementById(`${monthId}-card`);
+      // add active class if selectedMonth exist or is null
+      if (selectedMonthDiv) {
+        selectedMonthDiv.classList.add("active");
+      }
+    });
   });
 });
