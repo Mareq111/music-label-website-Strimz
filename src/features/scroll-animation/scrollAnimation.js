6cf2c3e758d0db40b4ea9fc3c1548121d985373c
marq sz <marekszulc2020@gmail.com>
2023-12-26 10:39:48 +0100

    fix: delete previous mistake,  feat: add scroll animation with intersection observers for albums into homepage

--- src/features/scroll-animation/scrollAnimation.js
+++ src/features/scroll-animation/scrollAnimation.js
@@ -1,47 +1,29 @@
+// 1 feature
 //!this function uses the Intersection Observer to give class to things when they appear in the browser window while scrolling
 //! it only happens once for everything on the page.
 
-//1. for albums - el double m on home page
-let observerExecuted = false;
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    if (!observerExecuted) {
-      entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-          entry.target.classList.add("album-show");
-          observerExecuted = true;
-        }
-      });
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("album-show");
+      observer.unobserve(entry.target);
     }
-  },
-  { once: true }
-);
+  });
+});
 
 const hiddenElements = document.querySelectorAll(".album-hidden");
-//observe all hidden elements
 hiddenElements.forEach((el) => observer.observe(el));
 
-//2. for albums - others musicians on home page
+//other musician albums
 
-let observerExecutedOthers = false;
-
-const observerOthers = new IntersectionObserver(
-  (entries) => {
-    if (!observerExecutedOthers) {
-      entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-          entry.target.classList.add("album-show-others");
-          observerExecutedOthers = true;
-        }
-      });
+const observerOthers = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("album-show-others");
+      observer.unobserve(entry.target);
     }
-  },
-  { once: true }
-);
+  });
+});
 
 const hiddenElementsOthers = document.querySelectorAll(".album-hidden-others");
-//observe all hidden elements
 hiddenElementsOthers.forEach((el) => observerOthers.observe(el));
