8d2a8ea36abdab017337d421cd19ef5858dc9b43
marq sz <marekszulc2020@gmail.com>
2024-01-04 16:04:43 +0100

    feat: add animation to slider , which is set up correct only for 320px screens

--- other-pages-script.js
+++ other-pages-script.js
@@ -213,3 +213,31 @@ document.addEventListener("keydown", function (event) {
 document.addEventListener("mousedown", function () {
   document.body.classList.remove("user-is-tabbing");
 });
+
+//! 11 slider into homepage
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.section-0-slider > div');
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    // hide all sliders
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('slider-active');
+      slides[i].classList.add('slider-hidden');
+    }
+
+    // show current slider
+    slides[index].classList.remove('slider-hidden');
+    slides[index].classList.add('slider-active');
+  }
+
+  function nextSlide() {
+    showSlide(currentSlide);
+    currentSlide = (currentSlide + 1) % slides.length;
+  }
+
+  //next slider per 4seconds
+  setInterval(nextSlide, 4000);
+});
